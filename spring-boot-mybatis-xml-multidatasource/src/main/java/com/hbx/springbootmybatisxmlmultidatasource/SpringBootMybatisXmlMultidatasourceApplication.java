69ed625b469ed47b43a9624a1e16d6b130b2baf6
huangbx <huangbx@example.com>
2019-05-25 19:41:51 +0800

    新增项目

---
+++ spring-boot-mybatis-xml-multidatasource/src/main/java/com/hbx/springbootmybatisxmlmultidatasource/SpringBootMybatisXmlMultidatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.hbx.springbootmybatisxmlmultidatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisXmlMultidatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisXmlMultidatasourceApplication.class, args);
+    }
+
+}
