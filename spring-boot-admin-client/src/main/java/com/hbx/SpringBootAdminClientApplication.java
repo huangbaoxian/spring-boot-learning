e89f545820aec8a0918e20a7d06a5fe1dff015a8
huangbx <huangbx@example.com>
2019-05-31 19:51:18 +0800

    新增client和server

---
+++ spring-boot-admin-client/src/main/java/com/hbx/SpringBootAdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.hbx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminClientApplication.class, args);
+    }
+
+}
