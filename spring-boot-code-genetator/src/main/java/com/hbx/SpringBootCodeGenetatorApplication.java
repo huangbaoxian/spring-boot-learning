c77f0bf666aad0420baaa2d7c4d54f7fdcec540d
huangbx <huangbx@example.com>
2019-05-31 17:13:13 +0800

    新增代码生成工程

---
+++ spring-boot-code-genetator/src/main/java/com/hbx/SpringBootCodeGenetatorApplication.java
@@ -0,0 +1,13 @@
+package com.hbx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCodeGenetatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCodeGenetatorApplication.class, args);
+    }
+
+}
